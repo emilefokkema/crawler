40f70ed17b27cd6006767c7f6d60612650165dc2
Emile Fokkema <emilefokkema@gmail.com>
2020-07-09 13:31:08 +0200

    improve the regex

--- Crawler/UrlFinderConsumer.cs
+++ Crawler/UrlFinderConsumer.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Text.RegularExpressions;
 using System;
+using System.Threading.Tasks;
 
 namespace Crawler
 {
@@ -9,10 +10,27 @@ namespace Crawler
     {
         private Regex _urlRegex;
         private readonly List<ILinkConsumer> _linkConsumers;
-        public UrlFinderConsumer(IEnumerable<ILinkConsumer> linkConsumers)
+        private readonly IColoredLineWriter _coloredLineWriter;
+        public UrlFinderConsumer(IEnumerable<ILinkConsumer> linkConsumers, IColoredLineWriter coloredLineWriter)
         {
-            _urlRegex = new Regex(@"https?://(?:[a-z]+\.)*[a-z]+(?:/[a-z]+)*/?");
+            _coloredLineWriter = coloredLineWriter;
+            _urlRegex = BuildUrlRegex();
             _linkConsumers = linkConsumers.ToList();
+        }
+
+        private static Regex BuildUrlRegex()
+        {
+            string domainNamePart = @"[0-9a-z_\-]+";
+            string domainName = $"(?:{domainNamePart}\\.)+{domainNamePart}";
+            string directory = @"[0-9a-z_\-]+";
+            string fileNameBeforeExtension = @"[0-9a-z_\-+]+";
+            string fileName = $"{fileNameBeforeExtension}(?:\\.[a-z0-9]+)?";
+            string path = $"(/{directory})*/(?:{fileName})?";
+            string queryParamName = @"[0-9a-z_\-]+";
+            string queryParamValue = @"[0-9a-z_\-]*";
+            string queryParam = $"{queryParamName}={queryParamValue}";
+            string queryString = $"\\?{queryParam}(?:&{queryParam})*";
+            return new Regex($"https?://{domainName}(?:{path})?(?:{queryString})?", RegexOptions.IgnoreCase);
         }
 
         public void Consume(Result result)
@@ -24,12 +42,20 @@ namespace Crawler
             IList<Match> matches = _urlRegex.Matches(success.Content);
             foreach (var match in matches)
             {
+                PrintMatchEnvironment(match, success.Content);
                 var link = new Link(success.Url, new Uri(match.Value));
                 foreach (var linkConsumer in _linkConsumers) 
                 {
                     linkConsumer.Consume(link);
                 }
             }
+        }
+
+        private void PrintMatchEnvironment(Match match, string source)
+        {
+            string before = source.Substring(match.Index - 5, 5);
+            string after = source.Substring(match.Index + match.Value.Length, 5);
+            _coloredLineWriter.WriteLine($"{before} [ {match.Value}  ]  {after}", ConsoleColor.Green);
         }
     }
 }
