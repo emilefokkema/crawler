38a9e68b2a75ec725eb8f670f41b41ff5e32d6bb
Emile Fokkema <emilefokkema@gmail.com>
2020-07-20 21:40:41 +0200

    step

--- test-web/build-parser.js
+++ test-web/build-parser.js
@@ -1,1 +1,18 @@
+var fsp = require('./fs-promise-wrapper');
 var peg = require("pegjs");
+
+(async function(){
+	var definition = await fsp.readFile('./parser-definition.peg', 'utf8');
+	var parser = peg.generate(definition);
+
+	var templateStrings = [
+		"a {{b}} c",
+		"if(a){{a {{b}} c}}",
+		"{{x}} !== {{x + 1}}"
+	];
+
+	for(var i = 0; i < templateStrings.length; i++){
+		var result = parser.parse(templateStrings[i]);
+		console.log(JSON.stringify(result));
+	}
+})();
