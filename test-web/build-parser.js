1ff2a803e6a83b64041cac3a0f006e77294c1e42
Emile Fokkema <emilefokkema@gmail.com>
2020-07-21 09:29:10 +0200

    step

--- test-web/build-parser.js
+++ test-web/build-parser.js
@@ -1,18 +1,51 @@
 var fsp = require('./fs-promise-wrapper');
 var peg = require("pegjs");
 
-(async function(){
-	var definition = await fsp.readFile('./parser-definition.peg', 'utf8');
-	var parser = peg.generate(definition);
+var parser = undefined;
+var whenTestsHaveRun;
 
-	var templateStrings = [
-		"a {{b}} c",
-		"if(a){{a {{b}} c}}",
-		"{{x}} !== {{x + 1}}"
+var createParser = async function(){
+	var definition = await fsp.readFile('./parser-definition.peg', 'utf8');
+	return peg.generate(definition);
+};
+
+var compileTemplate = async function(string){
+	if(!parser){
+		parser = await createParser();
+	}
+	var parsed = parser.parse(string);
+	console.log(parsed);
+};
+
+var testsPass = true;
+var testCases = [
+		{
+			template: "{{x}} !== {{x + 1}}",
+			data: {x : 2},
+			expectedResult: "2 !== 3"
+		}
 	];
 
-	for(var i = 0; i < templateStrings.length; i++){
-		var result = parser.parse(templateStrings[i]);
-		console.log(JSON.stringify(result));
+var runTest = async function(testCase, fail){
+	try{
+		var compiled = await compileTemplate(testCase.template);
+	}catch(e){
+		fail(e.message);
 	}
-})();
+};
+
+var runTests = async function(){
+	var fail = function(msg){
+		console.error(`test fails: ${msg}`);
+		testsPass = false;
+	}
+	await Promise.all(testCases.map(function(t){return runTest(t, fail);}));
+};
+
+whenTestsHaveRun = runTests();
+
+module.exports = async function(string){
+	await whenTestsHaveRun;
+	console.assert(testsPass);
+	return await compileTemplate(string);
+};
